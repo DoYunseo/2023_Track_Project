ecab5e203568f6c467bac9fb0002414deb365952
DESKNYAP\didgk <didgk0823@gmail.com>
2023-11-19 05:32:00 +0900

    add feat : modify extraction

--- flask_server.py
+++ flask_server.py
@@ -1,29 +1,34 @@
-from flask import Flask, request, response
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
 @app.route('/image/saveImage', methods = ['GET', 'POST'])
 def saveImage():
     if request.method == 'POST':
-        img = request.files['file']
-        img.save('static/image/' + secure_filename(img.filename))
-        return 'static/image/' + secure_filename(img.filename)
+        img = request.files['img']
+        name, extension = os.path.splitext(img.filename)
+        extension = '.png'
+        img.save('static/image/' + secure_filename(name) + extension)
+        return 'static/image/' + secure_filename(name) + extension
 
 @app.route('/video/saveVideo', methods = ['GET', 'POST'])
 def saveVideo():
     if request.method == 'POST':
-        vid = request.files['file']
-        vid.save('static/video/' + secure_filename(vid.filename))
-        return 'static/video/' + secure_filename(vid.filename)
+        vid = request.files['vid']
+        name, extension = os.path.splitext(vid.filename)
+        extension = '.mp4'
+        vid.save('static/video/' + secure_filename(name) + extension)
+        return 'static/video/' + secure_filename(name) + extension
 
 @app.route('/video/YouTubeVideo', methods = ['GET', 'POST'])
 def saveYouTube():
     if request.method == 'POST':
-        url = request.form['youtube_url']
+        url = request.form['url']
         ydl_opts = {
-        'format': 'best',
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best',
         'outtmpl': 'static/video/%(id)s.mp4'
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -34,10 +39,12 @@ def saveYouTube():
 @app.route('/record/saveRecord', methods = ['GET', 'POST'])
 def saveRecord():
     if request.method == 'POST':
-        rec = request.files['file']
-        rec.save('static/record/' + secure_filename(rec.filename))
-        return 'static/record/' + secure_filename(rec.filename)
+        rec = request.files['rec']
+        name, extension = os.path.splitext(rec.filename)
+        extension = '.m4a'
+        rec.save('static/record/' + secure_filename(name)) + extension
+        return 'static/record/' + secure_filename(name) + extension
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = 8080)
